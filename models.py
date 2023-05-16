70601391c6d4d318901b58675b61b2182e84b922
alanacabral <132599468+alanacabral@users.noreply.github.com>
2023-05-15 23:06:45 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
+#No models é onde estão as classes, que contém as informações sobre os dados do app. Dadas as informações fornecidas pelo models, ocorre a realização das migrações 
+#makemigrations que é o sistema que empacota as alterações feitas no models e migram elas para o resto do programa e migrate que é o sistema para aplicar as migrações 
+#guardadas no makemigrations no banco de dados.
+
+
 from django.db import models
 from django.utils import timezone
 
